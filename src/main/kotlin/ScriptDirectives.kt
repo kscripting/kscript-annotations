17b86b805b9ddb68437f097617f865a6ffc88f18
Holger Brandl <holgerbrandl@gmail.com>
2017-11-14 13:58:26 +0100

    Added @DependsOnMaven and @MavenRepository

--- src/main/kotlin/ScriptDirectives.kt
+++ src/main/kotlin/ScriptDirectives.kt
@@ -1,31 +1,48 @@
-@Target(allowedTargets = *arrayOf(AnnotationTarget.FILE))
+@Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
 @MustBeDocumented
 @Repeatable
-annotation class DependsOn(vararg val args: String)
+annotation class DependsOn(vararg val artifactLocators: String)
 
 // add repo to annotation
 //annotation class DependsOn(vararg val args : String, val from = "jcenter")
 
-@Target(allowedTargets = *arrayOf(AnnotationTarget.FILE))
+@Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
 @MustBeDocumented
 annotation class EntryPoint(val value: String)
 
 
-@Target(allowedTargets = *arrayOf(AnnotationTarget.FILE))
+@Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
 @MustBeDocumented
 @Repeatable
 annotation class Include(val includePath: String)
 
 
-@Target(allowedTargets = *arrayOf(AnnotationTarget.FILE))
+@Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
 @MustBeDocumented
 @Repeatable
 annotation class KotlinOpts(val runOptions: String)
 
 
+// MavenRepository and DependsOnMaven are supported "as it" for compatibility with jupyter notebooks.
+// see https://github.com/kohesive/keplin/blob/master/keplin-maven-resolver/src/main/kotlin/uy/kohesive/keplin/kotlin/script/resolver/maven/MavenResolverAnnotations.kt
+// example https://github.com/kohesive/keplin/blob/a307e05eae091c99c665fe3da52a428fb0e10a6a/keplin-maven-resolver/src/test/kotlin/uy/kohesive/keplin/kotlin/script/resolver/maven/TestMavenScriptDependencies.kt#L38
 
+
+// note: in contrasts to DependsOn DependsOnMaven just supports a single artifact argument
+@Target(AnnotationTarget.FILE)
+@Retention(AnnotationRetention.SOURCE)
+@Repeatable
+annotation class DependsOnMaven(val artifactLocator: String)
+
+@Target(AnnotationTarget.FILE)
+@Retention(AnnotationRetention.SOURCE)
+@Repeatable
+// in contrast to the original version we make the id mandatory here.
+// This keeps ensures compatiblity with keplin but eases parsing
+//annotation class MavenRepository(val id: String = "", val url: String)
+annotation class MavenRepository(val id: String, val url: String)
 
