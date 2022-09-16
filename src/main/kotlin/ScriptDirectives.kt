6ef4cf60382ece62aede9f173a4ca8f5ff86d099
Marcin Kuszczak <1508798+aartiPl@users.noreply.github.com>
2022-09-16 20:59:40 +0200

    Added @file:Import() directive; updated versions and library group.

--- src/main/kotlin/ScriptDirectives.kt
+++ src/main/kotlin/ScriptDirectives.kt
@@ -1,17 +1,15 @@
+@file:Suppress("unused")
+
 @Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
 @MustBeDocumented
 @Repeatable
 annotation class DependsOn(vararg val artifactLocators: String)
 
-// add repo to annotation
-//annotation class DependsOn(vararg val args : String, val from = "jcenter")
-
 @Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
 @MustBeDocumented
 annotation class EntryPoint(val value: String)
-
 
 @Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
@@ -19,6 +17,11 @@ annotation class EntryPoint(val value: String)
 @Repeatable
 annotation class Include(val includePath: String)
 
+@Target(AnnotationTarget.FILE)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+@Repeatable
+annotation class Import(val includePath: String)
 
 @Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
@@ -26,6 +29,11 @@ annotation class Include(val includePath: String)
 @Repeatable
 annotation class KotlinOpts(val runOptions: String)
 
+@Target(AnnotationTarget.FILE)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+@Repeatable
+annotation class KotlinOptions(val runOptions: String)
 
 @Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
@@ -33,11 +41,21 @@ annotation class KotlinOpts(val runOptions: String)
 @Repeatable
 annotation class CompilerOpts(val kotlincFlags: String)
 
+@Target(AnnotationTarget.FILE)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+@Repeatable
+annotation class CompilerOptions(val kotlincFlags: String)
+
+@Target(AnnotationTarget.FILE)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+@Repeatable
+annotation class Repository(val url: String)
 
 // MavenRepository and DependsOnMaven are supported "as it" for compatibility with jupyter notebooks.
 // see https://github.com/kohesive/keplin/blob/master/keplin-maven-resolver/src/main/kotlin/uy/kohesive/keplin/kotlin/script/resolver/maven/MavenResolverAnnotations.kt
 // example https://github.com/kohesive/keplin/blob/a307e05eae091c99c665fe3da52a428fb0e10a6a/keplin-maven-resolver/src/test/kotlin/uy/kohesive/keplin/kotlin/script/resolver/maven/TestMavenScriptDependencies.kt#L38
-
 
 // note: in contrasts to DependsOn DependsOnMaven just supports a single artifact argument
 @Target(AnnotationTarget.FILE)
@@ -45,14 +63,6 @@ annotation class CompilerOpts(val kotlincFlags: String)
 @Repeatable
 annotation class DependsOnMaven(val artifactLocator: String)
 
-/**
- * Declare a maven repository that will be used by kscript to resolve dependencies
- *
- * @param id A user-defined name for this repository
- * @param url The endpoint under which maven will find the repo
- * @param user Optional user name
- * @param password Optional password (required if user is defined)
- */
 @Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
 @Repeatable
